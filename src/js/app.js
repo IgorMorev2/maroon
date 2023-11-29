1921cb0a0c17852e386cf58b37a6f68a29dea433
IgorMorev <igor.morev2000@gmail.com>
2023-11-29 15:57:07 +0300

    Update 2023-11-29T12:56:51.781Z

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import "./modules/burger.js";
-import "./modules/sliders.js";
-import "./modules/map.js";
-import "./modules/spoiler.js";
-import "./modules/catalog-filter.js";
-
-import { useDynamicAdapt } from "./modules/dynamic-adapt.js";
-useDynamicAdapt();
-
