0bf063fec7533f4759cb3ee9ba3af32a38a234be
IgorMorev <igor.morev2000@gmail.com>
2024-01-10 12:36:38 +0300

    finished the catalog

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,41 +1,65 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination} from 'swiper/modules';
+import { Navigation, Pagination, Grid } from 'swiper/modules';
 
-const bestsellerSlider = new Swiper('.bestseller-slider', {
-  modules: [Navigation],
-  direction: 'horizontal',
-  spaceBetween: 30,
-  slidesPerView: 'auto',
-  grabCursor: true,
-  centeredSlides: true,
-  speed: 500,
-  mousewheel: true,
-  keyboard: {
-    enabled: true,
-  },
-  navigation: {
-    nextEl: '.navigation-products-list__button--next',
-    prevEl: '.navigation-products-list__button--prev',
-  },
-  breakpoints: {
-    768: {
-      centeredSlides: false,
+if (document.querySelector('.bestseller-slider')) {
+  const bestsellerSlider = new Swiper('.bestseller-slider', {
+    modules: [Navigation],
+    direction: 'horizontal',
+    spaceBetween: 30,
+    slidesPerView: 'auto',
+    grabCursor: true,
+    centeredSlides: true,
+    speed: 500,
+    mousewheel: true,
+    keyboard: {
+      enabled: true,
+    },
+    navigation: {
+      nextEl: '.navigation-products-list__button--next',
+      prevEl: '.navigation-products-list__button--prev',
+    },
+    breakpoints: {
+      768: {
+        centeredSlides: false,
+      }
     }
-  }
-});
+  });
+}
 
-const recommendationsSlider = new Swiper('.products-slider', {
-  modules: [Navigation, Pagination],
-  slidesPerView: 'auto',
-  spaceBetween: 50,
-  speed: 500,
-  navigation: {
-    nextEl: '.navigation-products-list__button--next',
-    prevEl: '.navigation-products-list__button--prev',
-  },
-  pagination: {
-    el: ".navigation-products-list__pagination",
-    type: "fraction",
-  },
-});
+if (document.querySelector('.recommendation-slider')) {
+  const recommendationSlider = new Swiper('.recommendation-slider', {
+    modules: [Navigation, Pagination, Grid],
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.navigation-products-list__button--next',
+      prevEl: '.navigation-products-list__button--prev',
+    },
+    pagination: {
+      el: '.navigation-products-list__pagination',
+      type: 'fraction',
+      renderFraction: (currentClass, totalClass) => {
+        return `<span class="${currentClass}"></span>` +
+          `<span class="${totalClass}"></span>`;
+      },
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        grid: {
+          rows: 2,
+          fill: 'row',
+        },
+        slidesPerGroup: 2,
+      },
+      1366: {
+        slidesPerView: 4,
+        grid: {
+          rows: 1,
+        },
+        slidesPerGroup: 1,
+      },
+    }
+  });
+}
 
